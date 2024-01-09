4dcc5a47989b5f4fd503f0d4a20ae3137887263b
Elisa Bothy <elisa.bothy@gmail.com>
2024-01-09 16:25:21 +0100

    j'ai oublié de push avant, ça ne marche pas, on est pdt la correction

---
+++ assets/js/dom.js
@@ -0,0 +1,9 @@
+const title = document.querySelector("title")[0];
+const li1 = document.getElementsByTagName("li")[0];
+const li2 = document.getElementsByTagName("li")[1];
+const a1 = li1.children[0];
+const a2 = li2.children[0];
+
+title.textContent = "Papa";
+a1.textContent = `Papa est en bas`;
+a2.textContent = "Qui fait du nougat";
